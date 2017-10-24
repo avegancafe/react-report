e0fddfe59d93cf97cc6a651fe08eff99bb205689
Kyle Holzinger <kylelholzinger@gmail.com>
2017-10-24 02:13:03 -0400

    [KH] example?

---
+++ example/index.js
@@ -0,0 +1,16 @@
+import {Container, Text} from '../'
+import {Component} from 'react'
+
+const someTerms = [
+  { id: 1, label: 'Deal Term 1' },
+  { id: 1, label: 'Deal Term 1' },
+  { id: 1, label: 'Deal Term 1' }
+]
+
+class DealTerms extends Component {
+  render() {
+    return <Container>
+      {someTerms.map(term => <Text>{term.label}</Text>)}
+    </Container>
+  }
+}
